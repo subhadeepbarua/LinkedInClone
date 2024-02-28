1712dff7424ba3a3b4e944cdbb027c8495d7c503
Subhadeep Barua <83444796+subhadeepbarua@users.noreply.github.com>
2024-02-28 19:32:37 +0530

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 // Connect to MongoDB using Mongoose
 mongoose.connect('mongodb+srv://LinkedInUserDB:K4VksmR4kicVHpzB@userdatabase.pbyxc6b.mongodb.net/LinkedIn?retryWrites=true&w=majority').then(() => {
-  console.log('Connected to MongoDB');
+  
 }).catch((err) => {
   console.error('Error connecting to MongoDB:', err);
 });
